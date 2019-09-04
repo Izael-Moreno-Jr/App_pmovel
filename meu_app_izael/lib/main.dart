6a7838c47566110874dd6b792c2d916ec3dac0c9
Izael Moreno Jr <izaelmorenojr17@gmail.com>
2019-09-04 16:33:15 -0300

    alteracao de Widget de calendario, sera usado o 'Date and Time pickers', foi add um botao para ir ate o Widget de calendario

--- meu_app_izael/lib/main.dart
+++ meu_app_izael/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:calendarro/date_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:calendarro/calendarro.dart';
+import 'dart:async';
 
 void main() => runApp(MaterialApp(home: Home()));
 
@@ -9,12 +10,33 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
+
+
 class _HomeState extends State<Home> {
+  DateTime _data = new DateTime.now();
+  TimeOfDay _time = new TimeOfDay.now();
+
+  Future<Null> _selecionarData(BuildContext context) async{
+    final DateTime escolhido = await showDatePicker(
+        context: context,
+        initialDate: _data,
+        firstDate: new DateTime(2017),
+        lastDate: new DateTime(2050)
+    );
+
+    if(escolhido != null && escolhido != _data){
+      print('data selecionada: ${_data.toString()}');
+      setState(() {
+        _data = escolhido;
+      });
+
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white70,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("Feriados nascionais"),
         centerTitle: true,
@@ -23,16 +45,21 @@ class _HomeState extends State<Home> {
       body: Column(
         children: <Widget>[
           Container(
-            child: Calendarro(
-              selectionMode: SelectionMode.SINGLE,
-              displayMode: DisplayMode.MONTHS,
-            ),
-          ),
-          Container(height: 32.0),
 
+            child: Column(
+              children: <Widget>[
+                new Text('data selecionada: ${_data.toString()}' ),
+                new RaisedButton(
+                    child: Text('selecione uma data'),
+                    onPressed: (){
+                      _selecionarData(context);
+                    })
+              ],
+            )
+          ),
         ],
       ),
-      drawer: Drawer(),
+
     );
   }
 }
