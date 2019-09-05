17caa3bc07a4c01746b20c5b4af853a2d8183ac4
Izael Moreno Jr <izaelmorenojr17@gmail.com>
2019-09-05 16:52:53 -0300

    adicao do codigo de carregamento e link da api

--- meu_app_izael/lib/main.dart
+++ meu_app_izael/lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:calendarro/date_utils.dart';
 import 'package:flutter/material.dart';
-import 'package:calendarro/calendarro.dart';
 import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 
 void main() => runApp(MaterialApp(home: Home()));
 
@@ -10,13 +11,12 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-
-
 class _HomeState extends State<Home> {
+
   DateTime _data = new DateTime.now();
   TimeOfDay _time = new TimeOfDay.now();
 
-  Future<Null> _selecionarData(BuildContext context) async{
+  Future _selecionarData(BuildContext context) async{
     final DateTime escolhido = await showDatePicker(
         context: context,
         initialDate: _data,
@@ -33,6 +33,16 @@ class _HomeState extends State<Home> {
     }
   }
 
+  Future<Map> pegarDados() async {
+    http.Response resposta = await http.get(
+        "https://api.calendario.com.br/?json=true&ano=$_data&ibge=3550308&token=aXphZWxtb3Jlbm9qckBnbWFpbC5jb20maGFzaD0xMjU1OTM0ODY"
+    );
+
+    return json.decode(resposta.body);
+
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,24 +52,44 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         backgroundColor: Colors.blueAccent,
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-
-            child: Column(
-              children: <Widget>[
-                new Text('data selecionada: ${_data.toString()}' ),
-                new RaisedButton(
-                    child: Text('selecione uma data'),
-                    onPressed: (){
-                      _selecionarData(context);
-                    })
-              ],
-            )
-          ),
-        ],
+      body: FutureBuilder<Map>(
+        future: pegarDados(),
+        builder: (context, estado){
+          switch (estado.connectionState){
+            case ConnectionState.waiting:
+            case ConnectionState.none:
+            return Center(
+              child: Text(
+                "Carragando...",
+                style: TextStyle(color: Colors.blueAccent , fontSize: 25.0),
+              ),
+            );
+            default:
+              if (estado.hasData) {
+                return Center(
+                  child: Text(
+                    "Erro ao Carragar os Dados :(",
+                    style: TextStyle(color: Colors.blueAccent, fontSize: 25.0),
+                  ),
+                );
+              } else{
+                return SingleChildScrollView(
+                  child:  Column(
+                    children: <Widget>[
+                      new Text('data selecionada: ${_data.toString()}' ),
+                      new RaisedButton(
+                          child: Text('selecione uma data'),
+                          onPressed: (){
+                            _selecionarData(context);
+                          }),
+                      new Text('')
+                    ],
+                  )
+                );
+              }
+          }
+        }
       ),
-
     );
   }
 }
